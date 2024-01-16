406a0927d8ce84fa5153f5f3f720d2e2b1cc94f5
USER <nurulqalbizahrani11@gmail.com>
2024-01-16 19:01:49 +0800

    progress

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function luas(){
+    var sisi = document.getElementById('Square').value;
+    var luas = sisi * sisi;
+    document.getElementById('luas').innerHTML = luas;
+    console.log(luas);
+}
+function kel(){
+    var sisi = document.getElementById('Square').value;
+    var kel = 4*sisi;
+    document.getElementById('kel').innerHTML = kel;
+    console.log(kel);
+}
