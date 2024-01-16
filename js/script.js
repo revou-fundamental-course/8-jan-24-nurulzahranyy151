6c911ece9f2ddbfc12cb4ed8d7354d13e1f0bae6
USER <nurulqalbizahrani11@gmail.com>
2024-01-16 22:48:00 +0800

    progress

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+let input=1234567890;
+try {
+    let user= INPUT.parse(input);
+} catch (err){
+    console.log("error name: ", err.name);
+    console.log("error message:", err.message);
+}
+
+
 function luas(){
     var sisi = document.getElementById('Square').value;
     var luas = sisi * sisi;
@@ -9,4 +18,8 @@ function kel(){
     var kel = 4*sisi;
     document.getElementById('kel').innerHTML = kel;
     console.log(kel);
+}
+
+function tampilkanOutput(){
+    
 }
